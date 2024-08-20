a542481dfdda17f088ece8817e51a85b5899d263
jack cruzan <jack@cruzans.com>
2024-08-20 09:01:46 -0700

    added new files and made player load as a triangle

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import pygame
 from constants import * 
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    FPS = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
@@ -11,7 +16,14 @@ def main():
                 return
 
         screen.fill("black")
+        dt = FPS.tick(60) / 1000
+        player.draw(screen)   
         pygame.display.flip()
+ 
+
+    
+
+
 
 if __name__ == "__main__":
     main()
